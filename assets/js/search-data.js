4288f67f9d21ef7bef0ae65a73987cab09d35828
zhenyiizhang <113996646+zhenyiizhang@users.noreply.github.com>
2025-06-14 12:59:00 +0000

    Deploying to gh-pages from @ zhenyiizhang/zhenyiizhang.github.io@ff9b8ca34b0ceeeac32d432286a6cc83718fbbb3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -71,6 +71,13 @@ ninja.data = [{
           window.open("https://scholar.google.com/citations?user=LuaNHYsAAAAJ", "_blank");
         },
       },{
+        id: 'social-semanticscholar',
+        title: 'Semantic Scholar',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.semanticscholar.org/author/2323728020", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
